cbef79ef8f121c85ed44a5e3d6b3a5274c820c19
Ujjawal Solanki <134290905+ujjawalsol@users.noreply.github.com>
2025-06-12 02:10:32 +0530

    error solve app.tsx

--- client/src/lib/protected-route.tsx
+++ client/src/lib/protected-route.tsx
@@ -7,7 +7,7 @@ export function ProtectedRoute({
   component: Component,
 }: {
   path: string;
-  component: () => React.JSX.Element;
+  component: () => React.JSX.Element | null;
 }) {
   const { user, isLoading } = useAuth();
 
